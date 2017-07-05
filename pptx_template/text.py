727f1a1c4bb32509eaefb1db838cda68b07b5bb3
reki2000 <r-murakami@m3.com>
2017-07-05 13:01:15 +0900

    More style-broken-free text substitution (#14)

--- pptx_template/text.py
+++ pptx_template/text.py
@@ -66,17 +66,23 @@ def replace_el_in_text_frame_with_str(text_frame, el, replacing_text):
   """
   placeholder = el_to_placeholder(el)
   for paragraph in text_frame.paragraphs:
-    if placeholder in paragraph.text:
-        for run in paragraph.runs:
-           if placeholder in run.text:
-             log.info(u"found text_id {%s}. replacing with: %s" % (el, replacing_text))
-             run.text = run.text.replace(placeholder, replacing_text)
-             return True
+    if not placeholder in paragraph.text:
+        continue
 
-        # if not found, do force replacement with breaking its font style
-        log.warning(u"found text_id {%s} but requires font style break. replacing with: %s" % (el, replacing_text))
-        paragraph.text = paragraph.text.replace(placeholder, replacing_text)
-        return True
+    log.debug(u"replacing runs: %s" % ([r.text for r in paragraph.runs]))
+    ((start_run, start_pos), (end_run, end_pos)) = find_el_position([r.text for r in paragraph.runs], el)
+    for (i, run) in enumerate(paragraph.runs):
+        if i == start_run and i == end_run:
+            run.text = run.text.replace(placeholder, replacing_text)
+        elif i == start_run:
+            run.text = run.text[0:start_pos] + replacing_text
+        elif i == end_run:
+            run.text = run.text[end_pos + 1:]
+            break
+        elif start_run < i and i < end_run:
+            run.text = ''
+    log.debug(u"replaced runs: %s" % ([r.text for r in paragraph.runs]))
+    return True
   return False
 
 def replace_all_els_in_text_frame(text_frame, model):
@@ -96,3 +102,34 @@ def replace_all_els_in_text_frame(text_frame, model):
 
     if not replace_el_in_text_frame_with_str(text_frame, el, replacing_text):
       log.error(u"Cannot find {%s} in one text-run. To fix this, select this whole EL [%s] and reset font size by clicking size up then down" % (text_id, text_frame.text))
+
+def find_el_position(texts, el):
+    """
+    text の配列中に分かれて記述されている EL の、先頭の '\{' の位置と、最後の '\}' の位置を返す。
+    それぞれの位置は (text_index, position_in_text) の形で返される。
+    """
+    placeholder = el_to_placeholder(el)
+    full_text = ''.join(texts)
+
+    start_pos = full_text.find(placeholder)
+    if start_pos < 0:
+        raise ValueError(u"texts %s doesn't contain el:%s" % (texts, el))
+    end_pos   = start_pos + len(placeholder) - 1
+
+    start_run_pos = start_run_index = end_run_pos = end_run_index = -1
+    for (run_index, text) in enumerate(texts):
+        length = len(text)
+
+        # log.error("run_index:%(run_index)s start_pos:%(start_pos)s end_pos:%(end_pos)s length:%(length)s" % locals())
+        if start_pos >= 0 and start_pos < length:
+            start_run_index = run_index
+            start_run_pos = start_pos
+        start_pos = start_pos - length
+
+        if end_pos >= 0 and end_pos < length:
+            end_run_index = run_index
+            end_run_pos = end_pos
+            break
+        end_pos = end_pos - length
+
+    return ((start_run_index, start_run_pos), (end_run_index, end_run_pos))
